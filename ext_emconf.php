a97a098b87f21328ee4b524124caf15042f69789
Sebastian Richter <baschte@users.noreply.github.com>
2024-09-02 12:21:44 +0200

    [TASK] TYPO3 12 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,14 +3,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'powermail_limits',
     'description' => 'Addition to EXT:powermail which allows limits for form submissions',
     'category' => 'plugin',
-    'version' => '1.0.3',
-    'state' => 'stable',
+    'version' => '1.1.0',
+    'state' => 'beta',
     'author' => 'Schuler, J. Peter M.',
     'author_email' => 'j.peter.m.schuler@uni-due.de',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99'
+            'typo3' => '11.5.0-12.4.99'
         ]
     ]
 ];
