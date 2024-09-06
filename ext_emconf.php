c3ead3aed4138583af9011f11605ae3bcf196fb5
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2024-09-06 11:55:44 +0200

    [REGRESSION] remove TER compat info for 11LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,13 +5,13 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Addition to EXT:powermail which allows limits for form submissions',
     'category' => 'plugin',
     'version' => '2.0.0',
-    'state' => 'beta',
+    'state' => 'stable',
     'author' => 'Schuler, J. Peter M.',
     'author_email' => 'j.peter.m.schuler@uni-due.de',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99'
+            'typo3' => '12.4.0-12.4.99'
         ]
     ]
 ];
